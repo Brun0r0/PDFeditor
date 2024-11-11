0bf661f9bd8d598075484dd45eac39a65ece5e9a
Brun0r0 <gabrielbrunoro55@gmail.com>
2024-11-10 22:37:15 -0300

    Melhoramento de organização e updates no Juntar

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter.filedialog import askopenfilenames
 from tkinter import messagebox
 import PyPDF2    
+from PIL import Image, ImageTk
 
 class App(tk.Tk):
 
@@ -11,118 +12,126 @@ class App(tk.Tk):
         super().__init__()
         self.title(title)
         self.geometry(f'{size[0]}x{size[1]}') 
-        self.minsize(size[0],size[1])
-        
-        #widgets
-        self.menu = Menu(self)
+        self.resizable(False, False)
 
+        self.currentFrame = None
+
+        self.showFrame(Menu)
+        
         #run
         self.mainloop()
+
+
+    def showFrame(self, frame_class):
+        if self.currentFrame is not None:
+            self.currentFrame.destroy()
+
+        self.currentFrame = frame_class(self)
+        self.currentFrame.place(x=0, y=0, relwidth=1, relheight=1)
 
 class Menu(tk.Frame):
     def __init__(self, parent):
         super().__init__(parent)
-        self.place(x=0, y=0, relheight=1, relwidth=1)
+        self.parent = parent
+        self.configure(background='gray')
+
         self.createWidgets()
 
 
-#OBS: isso aqui está ruim mesmo, somente salvando o progresso para a partir de mais estudos aperfeiçoar
-        
-    def janelaJuntar(self):
-
-        def arqUp():
-            posList = box_arq.curselection()
-            
-            if not posList:
-                return
-
-            for pos in posList:
-                if pos == 0:
-                    continue
-
-                text = box_arq.get(pos)
-                box_arq.delete(pos)
-                box_arq.insert(pos-1, text)
-
-        def arqDown():
-            posList = box_arq.curselection()
-            
-            if not posList:
-                return
-
-            for pos in posList:
-                if pos == box_arq.size() - 1:
-                    continue
-
-                text = box_arq.get(pos)
-                box_arq.delete(pos)
-                box_arq.insert(pos+1, text)
-
-
-        def deletarSel():
-            box_arq.delete(tk.ACTIVE)
-
-        def selecionarArq():
-            arquivo_select = askopenfilenames(title='Selecione o PDF')
-            nome_arquivo = arquivo_select[0]
-            if(nome_arquivo[-1] == 'f' and nome_arquivo[-2] == 'd' and nome_arquivo[-3] == 'p' and nome_arquivo[-4] == '.'):
-                box_arq.insert(tk.END, arquivo_select)
-            elif(arquivo_select != ''):
-                messagebox.showerror('Type Error', 'somente arquivos do tipo PDF')
-            
-
-        def GrudarPDF():
-            lista = box_arq.get(0, tk.END)
-            lista_arquivos = list(lista)
-            tamList = len(lista_arquivos)
-            if(tamList == 0):
-                messagebox.showerror('NULL', 'Lista vazia')
-            else:
-                merger = PyPDF2.PdfMerger()
-                for arq in lista_arquivos:
-                    merger.append(arq[0])
-                merger.write("PDF Grudado.pdf")
-                messagebox.showinfo('Concluído', 'Foi realizada a junção')
-                janela2.quit()
-
-        janela2 = tk.Toplevel()
-        janela2.title("Juntador de PDFs")
-        janela2.minsize(width=500, height=500)
-        
-        label_entrada = tk.Label(janela2, text='Selecione os arquivos que deseja juntar e deixe na sequencia correta')
-        label_entrada.place(relx=0.15, rely=0.05)
-
-        buttom_sel = tk.Button(janela2, text='Selecionar Arquivo', command= selecionarArq)
-        buttom_sel.place(relx=0.4, rely=0.2)
-
-        box_arq = tk.Listbox(janela2, height = 15, width=50)
-        box_arq.place(relx=0.21, rely=0.3)
-
-        buttom_conc = tk.Button(janela2, text="Grudar", command= GrudarPDF)
-        buttom_conc.place(relx=0.3, rely=0.8)
-
-        buttom_del = tk.Button(janela2, text='Deletar', command=deletarSel)
-        buttom_del.place(relx=0.7, rely=0.8)
-
-        buttom_pos_up = tk.Button(janela2, text='↑', command=arqUp)
-        buttom_pos_up.place(relx=0.85, rely=0.5)
-
-        buttom_pos_down = tk.Button(janela2, text='↓', command=arqDown)
-        buttom_pos_down.place(relx=0.85, rely=0.6)
-
     def createWidgets(self):
-        menu_button1 = tk.Button(self, text='Juntar', command= self.janelaJuntar)
-        menu_button2 = tk.Button(self, text='Converter')
-        menu_button3 = tk.Button(self, text='Editar')
-        menu_button4 = tk.Button(self, text='Remover paginas')
-        menu_button5 = tk.Button(self, text='Separar paginas')
+        tk.Button(self, text='Juntar', command=lambda: self.parent.showFrame(Join)).pack(pady=30)
+        tk.Button(self, text='Separar paginas').pack(pady=30)
+        tk.Button(self, text='Remover paginas').pack(pady=30)
+        tk.Button(self, text='Converter').pack(pady=30)
+        tk.Button(self, text='Editar').pack(pady=30)
 
-        menu_button1.place(relx=0.35, rely=0.1)
-        menu_button2.place(relx=0.35, rely=0.18)
-        menu_button3.place(relx=0.35, rely=0.26)
-        menu_button4.place(relx=0.35, rely=0.34)
-        menu_button5.place(relx=0.35, rely=0.42)
 
-    
+class Join(tk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.parent = parent
+        self.configure(background='gray')
+        self.createWidgets()
 
-App('PDFediter', (400,400))
+        #ListBox
+        self.listBox = tk.Listbox(self, height = 20, width=58)
+        self.listBox.place(relx=0.15, rely=0.2)
+
+    #widgets
+    def createWidgets(self):
+        tk.Label(self, text='Selecione os arquivos que deseja juntar e deixe na sequência correta', background='gray').place(relx=0.15, rely=0.05)
+        
+        tk.Button(self, text='Selecionar Arquivo', command= self.selecionarArq).place(relx=0.4, rely=0.12)
+
+        tk.Button(self, text='Limpar', command= lambda: self.listBox.delete(0, tk.END), background='red2').place(relx=0.5, rely=0.86)
+
+        tk.Button(self, text='Deletar', command= lambda: self.listBox.delete(tk.ACTIVE), background='firebrick1').place(relx=0.63, rely=0.86)
+
+        tk.Button(self, text="Juntar", command= self.GrudarPDF, background='lime green').place(relx=0.76, rely=0.86)
+
+        tk.Button(self, text='↑', command=self.arqUp).place(relx=0.86, rely=0.47)
+
+        tk.Button(self, text='↓', command=self.arqDown).place(relx=0.86, rely=0.52)
+
+        tk.Button(self, text='Voltar', command= lambda: self.parent.showFrame(Menu)).place(relx=0.02, rely=0.92)
+
+    #funcs
+    def selecionarArq(self):
+        arquivo_select = askopenfilenames(title='Selecione o PDF')
+        nome_arquivo = arquivo_select[0]
+        if(nome_arquivo.lower().endswith('.pdf')):
+            self.listBox.insert(tk.END, nome_arquivo)
+        elif(arquivo_select):
+            messagebox.showerror('Type Error', 'somente arquivos do tipo PDF')
+
+
+    def arqUp(self):
+        posList = self.listBox.curselection()
+        
+        if not posList:
+            return
+
+        for pos in posList:
+            if pos == 0:
+                continue
+
+            text = self.listBox.get(pos)
+            self.listBox.delete(pos)
+            self.listBox.insert(pos-1, text)
+
+
+    def arqDown(self):
+        posList = self.listBox.curselection()
+        
+        if not posList:
+            return
+
+        for pos in posList:
+            if pos == self.listBox.size() - 1:
+                continue
+
+            text = self.listBox.get(pos)
+            self.listBox.delete(pos)
+            self.listBox.insert(pos+1, text)
+
+        
+    def GrudarPDF(self):
+        lista = self.listBox.get(0, tk.END)
+        lista_arquivos = list(lista)
+
+        if(lista_arquivos):
+            merger = PyPDF2.PdfMerger()
+
+            for arq in lista_arquivos:
+                merger.append(arq)
+
+            merger.write("PDF Gerado.pdf")
+
+            messagebox.showinfo('Concluído', 'Foi realizada a junção')
+            self.listBox.delete(0, tk.END)
+        
+        else:
+            messagebox.showerror('NULL', 'Lista vazia')
+
+
+App('PDFeditor', (500,500))
